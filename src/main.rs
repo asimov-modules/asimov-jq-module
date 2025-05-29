2099f6e7785e7a9ceb60d06878d1443667f621d3
Arto Bendiken <arto@asimov.so>
2025-05-29 09:03:52 +0400

    Implement the basic functionality.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,17 @@
 // This is free and unencumbered software released into the public domain.
 
 #[cfg(feature = "std")]
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+fn main() -> Result<clientele::SysexitsError, Box<dyn std::error::Error>> {
+    use clientele::SysexitsError::*;
+    use jq::JsonFilter;
+    use std::io::{Read, stdin, stdout};
+
+    // Load environment variables from `.env`:
+    clientele::dotenv().ok();
+
+    // Expand wildcards and @argfiles:
+    let args = clientele::args_os()?;
+
     // Configure tracing & logging:
     #[cfg(feature = "tracing")]
     tracing_subscriber::fmt()
@@ -9,9 +19,25 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .with_max_level(tracing_subscriber::filter::LevelFilter::WARN)
         .init();
 
-    println!("asimov-jq-runner"); // TODO
+    // Load and compile the filter:
+    let filter_path = args.iter().skip(1).next().expect("filter path");
+    let filter_json = std::fs::read_to_string(filter_path)?;
+    let filter: JsonFilter = filter_json.parse()?;
 
-    Ok(())
+    // Read the input JSON from stdin:
+    let mut input_json = String::new();
+    stdin().read_to_string(&mut input_json)?;
+
+    // Write the output JSON to stdout:
+    let output_json = filter.filter_json_str(input_json)?;
+    if cfg!(feature = "pretty") {
+        colored_json::write_colored_json(&output_json, &mut stdout())?;
+        println!();
+    } else {
+        println!("{}", serde_json::to_string(&output_json).unwrap());
+    }
+
+    Ok(EX_OK)
 }
 
 #[cfg(not(feature = "std"))]
